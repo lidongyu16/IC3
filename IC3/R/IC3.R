1752a9dd0726ad499bfec82f9cce8fb59a505d37
lidongyu16 <103477118+lidongyu16@users.noreply.github.com>
2023-12-06 16:07:15 -0800

    Update IC3.R

--- IC3/R/IC3.R
+++ IC3/R/IC3.R
@@ -7,7 +7,7 @@
 #' @param minitr The minimum iteration number. Default is 10.
 #' @param maxitr The maximum iteration number. Default is 100.
 #' @param minbeta The minimum value of parameter beta. Default is 0.
-#' @return The first term: the communication probability with cell type level; The second term: the communication probability with single cell level. The 3-5th term: parameter estimation of lambda;beta;r=(r0,r1,r2). The 6th term: log likelihood after each iteration
+#' @return The first term: the communication probability with cell type level; The second term: the communication probability with single cell level. The 3-5th term: parameter estimation of lambda;beta;r=(r0,r1,r2). The 6th term: Q function value after each iteration
 #' @export
 #'
 #' @examples IC3(A, cellinfo, lrinfo)

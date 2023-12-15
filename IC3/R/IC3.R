7a21f2c9e4da008d5d5743a14f8eda23492e0852
lidongyu16 <103477118+lidongyu16@users.noreply.github.com>
2023-12-14 22:50:42 -0800

    Update IC3.R

--- IC3/R/IC3.R
+++ IC3/R/IC3.R
@@ -550,7 +550,7 @@ IC3 <- function(A, cellinfo, lrinfo, alpha = 0.02, minitr = 10, maxitr = 100, mi
       if (chabeta < 0.1 && chalambda < 0.01 && char < 0.01 && chaECI < 0.01) {
         break
       }
-      if ((lljilu[itr] - lljilu[itr - 1]) < 0.2 && (lljilu[itr - 1] - lljilu[itr - 2]) < 0.2) {
+      if ((lljilu[itr] - lljilu[itr - 1]) < 0.01 ) {
         break
       }
     }
